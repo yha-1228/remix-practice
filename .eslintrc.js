b0e5f66eb6854d999f304e1506cfe44f23c7f08e
yuta <gssbtsm999@gmail.com>
2023-05-08 00:58:53 +0900

    chore(eslint): add tailwind rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,13 @@ module.exports = {
     "@remix-run/eslint-config",
     "@remix-run/eslint-config/node",
     "@remix-run/eslint-config/jest-testing-library",
+    "plugin:tailwindcss/recommended",
     "prettier",
   ],
   env: {
     "cypress/globals": true,
   },
-  plugins: ["cypress"],
+  plugins: ["cypress", "tailwindcss"],
   // we're using vitest which has a very similar API to jest
   // (so the linting plugins work nicely), but it means we have to explicitly
   // set the jest version.
